bfe37df7680bfa967ea9fd24dcb886c3ee210758
Trinh Van Dien <dientrinhsy82@gmail.com>
2019-03-25 17:08:56 +0700

    add test case

---
+++ test/testcheckbarcode.js
@@ -0,0 +1,11 @@
+var assert = require('chai').assert;
+
+describe('Test check generate barcode number', function() {
+    this.timeout(15000);  
+      it('should add numbers', function() {
+    // add an assertion
+	var numb=15;
+	assert.equal(numb, 15);
+  })
+});
+
